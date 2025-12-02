8ab8c18032a73bccdab4b9064ea60514365445f9
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-12-02 16:06:06 +0530

    directpayment comments update

--- processor/src/services/mock-payment.service.ts
+++ processor/src/services/mock-payment.service.ts
@@ -786,7 +786,7 @@ public async updateTxComment(paymentId: string, txId: string, comment: string) {
 
     const pspReference = randomUUID().toString();
     // Generate transaction comments
-    const transactionComments = `Novalnet Transaction ID: ${parsedResponse?.transaction?.tid ?? "N/A"}\nPayment Type: ${parsedResponse?.transaction?.payment_type ?? "N/A"}\n${JSON.stringify(parsedResponse) ?? "N/A"}`;
+    const transactionComments = `Novalnet Transaction ID: ${parsedResponse?.transaction?.tid ?? "N/A"}\nPayment Type: ${parsedResponse?.transaction?.payment_type ?? "N/A"}\n${transactiondetails ?? "N/A"}\n${bankDetails ?? ""}`;
     log.info("Payment created with Novalnet details for direct:");
     log.info("Payment transactionComments for direct:", transactionComments);
     log.info("ctPayment id for direct:", ctPayment.id);
