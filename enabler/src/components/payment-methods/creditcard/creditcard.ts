ae35fb1519b8e5ecf02f806b3eff7e7458ed1e2a
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-12-10 18:07:41 +0530

    removed clientkey code

--- enabler/src/components/payment-methods/creditcard/creditcard.ts
+++ enabler/src/components/payment-methods/creditcard/creditcard.ts
@@ -94,8 +94,7 @@ export class Creditcard extends BaseComponent {
       console.log("UNIQUE ID:", uniqueId);
       console.log("DO REDIRECT:", doRedirect);
       console.log('client-key');
-      const clientKeyValue = getConfig().novalnetClientkey ?? 'empty';
-      console.log(clientKeyValue);
+
       if (!panhash || !uniqueId) {
         this.onError("Credit card information is missing or invalid.");
         return;
