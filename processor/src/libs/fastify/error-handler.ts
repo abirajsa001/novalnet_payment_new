f8339a2326ca4b3b5c84b13001459c769a0ccfc2
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-12-24 12:57:31 +0530

    Delete processor directory

--- processor/src/libs/fastify/error-handler.ts
+++
@@ -1,3 +0,0 @@
-export const errorHandler = (err: any, _req: any, reply: any) => {
-  reply.status(500).send({ message: err?.message ?? 'error' });
-};
--- processor/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { setupFastify } from './server/server';
-
-(async () => {
-  const server = await setupFastify();
-  const port = Number(process.env.PORT || 8080);
-  await server.listen({ port, host: '0.0.0.0' });
-})();
